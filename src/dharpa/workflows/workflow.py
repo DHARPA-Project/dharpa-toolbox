1340f20c28111c480026287879a662f4dc9751dd
Markus Binsteiner <markus@frkl.io>
2021-03-02 10:35:45 +0100

    chore: import removed

--- src/dharpa/workflows/workflow.py
+++ src/dharpa/workflows/workflow.py
@@ -2,10 +2,9 @@
 import json
 import os
 import typing
+import yaml
 from pathlib import Path
 
-import yaml
-import zmq
 from dharpa.data.core import DataItem, DataSchema
 from dharpa.defaults import MODULE_TYPE_KEY
 from dharpa.models import (
@@ -20,126 +19,123 @@ from dharpa.models import (
 )
 from dharpa.processing.executors import Processor
 from dharpa.processing.processing_module import ProcessingModule
-from dharpa.workflows.events import ModuleEvent, ModuleEventType
 from dharpa.workflows.modules import InputItems, OutputItems, WorkflowModule
 from dharpa.workflows.structure import WorkflowStructure
-from dharpa.workflows.utils import create_workflow_modules
 
-
-class AssembledWorkflowInteractive(object):
-    def __init__(
-        self,
-        *module_configs: typing.Mapping[str, typing.Any],
-        workflow_id: str = None,
-        init_inputs: typing.Optional[InputItems] = None,
-    ):
-
-        self._module_configs: typing.Iterable[
-            typing.Mapping[str, typing.Any]
-        ] = module_configs
-        self._workflow_id: typing.Optional[str] = workflow_id
-        self._structure: WorkflowStructure = None  # type: ignore
-        # self._inputs: InputItems = inputs
-        # self._outputs: OutputItems = outputs
-
-        self._zmq_context: zmq.Context = zmq.Context.instance()
-        self._module_event_socket: zmq.Socket = self._zmq_context.socket(zmq.PULL)
-        self._module_event_socket.bind(f"inproc://{self._workflow_id}")
-        self._module_event_socket.bind("tcp://127.0.0.1:5555")
-
-        self._init_obj(init_inputs=init_inputs)
-
-    def _init_obj(self, init_inputs: typing.Optional[InputItems]) -> None:
-
-        m = create_workflow_modules(*self._module_configs)
-        self._structure = WorkflowStructure(*m, workflow_id=self._workflow_id)
-
-        structure_inputs: InputItems = InputItems(
-            **self._structure.workflow_input_schema
-        )
-        structure_outputs: OutputItems = OutputItems(
-            **self._structure.workflow_output_schema
-        )
-
-        for module_id, module_details in self._structure.module_details.items():
-
-            module_inputs = module_details["inputs"]
-            module_obj: WorkflowModule = module_details["workflow_module"]
-
-            for input_name, link in module_inputs.items():
-                connected_item = link.connected_item
-                input_item = module_obj.inputs[input_name]
-
-                if connected_item.link_type == "workflow_input":
-                    workflow_input: DataItem = structure_inputs[
-                        connected_item.value_name
-                    ]
-                    workflow_input.add_callback(input_item.set_value)
-                    if init_inputs:
-                        outer_workflow_input: DataItem = init_inputs[
-                            connected_item.value_name
-                        ]
-                        workflow_input.value = outer_workflow_input.value
-                else:
-                    module_id = connected_item.module_id
-                    value_name = connected_item.value_name
-                    other = self._structure.get_module(module_id)
-                    output_item = other.outputs[value_name]
-                    output_item.add_callback(input_item.set_value)
-
-            module_outputs = module_details["outputs"]
-            for output_name, link in module_outputs.items():
-                output_item = module_obj.outputs[output_name]
-
-                for wo in link.workflow_output:
-                    wo_item = structure_outputs[wo.value_name]
-                    output_item.add_callback(wo_item.set_value)
-
-        self._inputs = structure_inputs
-        self._outputs = structure_outputs
-
-        pass
-
-    @property
-    def inputs(self) -> InputItems:
-        return self._inputs
-
-    @property
-    def outputs(self) -> OutputItems:
-        return self._outputs
-
-    def listen(self) -> None:
-
-        try:
-            while True:
-                # print("wating")
-                event_data = self._module_event_socket.recv_json()
-                event = ModuleEvent.from_dict(**event_data)
-
-                # print(f"NEW EVENT: {event.to_dict()}")
-
-                if event.event_type == ModuleEventType.set_input:
-                    module_id = event.event_obj.module_id
-                    if module_id != self._workflow_id:
-                        raise NotImplementedError()
-
-                    input_name = event.event_obj.input_name
-                    value = event.event_obj.value
-
-                    self._set_input(input_name=input_name, value=value)
-
-                else:
-                    raise NotImplementedError()
-
-        except Exception as e:
-            print(e)
-
-        print("EXIT")
-
-    def _set_input(self, input_name: str, value: typing.Any):
-
-        inp = self.inputs[input_name]
-        inp.value = value
+# class AssembledWorkflowInteractive(object):
+#     def __init__(
+#         self,
+#         *module_configs: typing.Mapping[str, typing.Any],
+#         workflow_id: str = None,
+#         init_inputs: typing.Optional[InputItems] = None,
+#     ):
+#
+#         self._module_configs: typing.Iterable[
+#             typing.Mapping[str, typing.Any]
+#         ] = module_configs
+#         self._workflow_id: typing.Optional[str] = workflow_id
+#         self._structure: WorkflowStructure = None  # type: ignore
+#         # self._inputs: InputItems = inputs
+#         # self._outputs: OutputItems = outputs
+#
+#         self._zmq_context: zmq.Context = zmq.Context.instance()
+#         self._module_event_socket: zmq.Socket = self._zmq_context.socket(zmq.PULL)
+#         self._module_event_socket.bind(f"inproc://{self._workflow_id}")
+#         self._module_event_socket.bind("tcp://127.0.0.1:5555")
+#
+#         self._init_obj(init_inputs=init_inputs)
+#
+#     def _init_obj(self, init_inputs: typing.Optional[InputItems]) -> None:
+#
+#         m = create_workflow_modules(*self._module_configs)
+#         self._structure = WorkflowStructure(*m, workflow_id=self._workflow_id)
+#
+#         structure_inputs: InputItems = InputItems(
+#             **self._structure.workflow_input_schema
+#         )
+#         structure_outputs: OutputItems = OutputItems(
+#             **self._structure.workflow_output_schema
+#         )
+#
+#         for module_id, module_details in self._structure.module_details.items():
+#
+#             module_inputs = module_details["inputs"]
+#             module_obj: WorkflowModule = module_details["workflow_module"]
+#
+#             for input_name, link in module_inputs.items():
+#                 connected_item = link.connected_item
+#                 input_item = module_obj.inputs[input_name]
+#
+#                 if connected_item.link_type == "workflow_input":
+#                     workflow_input: DataItem = structure_inputs[
+#                         connected_item.value_name
+#                     ]
+#                     workflow_input.add_callback(input_item.set_value)
+#                     if init_inputs:
+#                         outer_workflow_input: DataItem = init_inputs[
+#                             connected_item.value_name
+#                         ]
+#                         workflow_input.value = outer_workflow_input.value
+#                 else:
+#                     module_id = connected_item.module_id
+#                     value_name = connected_item.value_name
+#                     other = self._structure.get_module(module_id)
+#                     output_item = other.outputs[value_name]
+#                     output_item.add_callback(input_item.set_value)
+#
+#             module_outputs = module_details["outputs"]
+#             for output_name, link in module_outputs.items():
+#                 output_item = module_obj.outputs[output_name]
+#
+#                 for wo in link.workflow_output:
+#                     wo_item = structure_outputs[wo.value_name]
+#                     output_item.add_callback(wo_item.set_value)
+#
+#         self._inputs = structure_inputs
+#         self._outputs = structure_outputs
+#
+#         pass
+#
+#     @property
+#     def inputs(self) -> InputItems:
+#         return self._inputs
+#
+#     @property
+#     def outputs(self) -> OutputItems:
+#         return self._outputs
+#
+#     def listen(self) -> None:
+#
+#         try:
+#             while True:
+#                 # print("wating")
+#                 event_data = self._module_event_socket.recv_json()
+#                 event = ModuleEvent.from_dict(**event_data)
+#
+#                 # print(f"NEW EVENT: {event.to_dict()}")
+#
+#                 if event.event_type == ModuleEventType.set_input:
+#                     module_id = event.event_obj.module_id
+#                     if module_id != self._workflow_id:
+#                         raise NotImplementedError()
+#
+#                     input_name = event.event_obj.input_name
+#                     value = event.event_obj.value
+#
+#                     self._set_input(input_name=input_name, value=value)
+#
+#                 else:
+#                     raise NotImplementedError()
+#
+#         except Exception as e:
+#             print(e)
+#
+#         print("EXIT")
+#
+#     def _set_input(self, input_name: str, value: typing.Any):
+#
+#         inp = self.inputs[input_name]
+#         inp.value = value
 
 
 class AssembledWorkflowBatch(object):
